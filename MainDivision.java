ca5e183a72ec496efb6a4b2d4b9d22f9cf60fc04
NeruChan2003 <167128377+NeruChan2003@users.noreply.github.com>
2024-05-02 20:53:14 -0600

    Division version 2
    Division de dos numeros version 2

--- MainDivision.java
+++ MainDivision.java
@@ -1,31 +1,33 @@
 
-// se importa la libreria Scanner
 import java.util.Scanner;
 
-// se declara la clase
 public class MainDivision {
-    // se agrega el metodo main
     public static void main(String[] args) {
-        // se llama al metodo Scanner
-        Scanner n1 = new Scanner(System.in);
-        // se ingresa el mensaje que se le dara al usuario
-        Division d1 = new Division();
-        System.out.println("Ingresa un numero");
-        // se almacena el valor del usuario
-        int a = n1.nextInt();
-        // se almacena el valor del usuario
-        System.out.println("Ingrese el segundo numero");
-        // se almacena el valor del usuario
-        int b = n1.nextInt();
-        // se hace la operacion con los datos ingresados por el usuario
-        int r = a / b;
-        // se imprime el resultado de la operacion y se muestra en pantalla
-        d1.a = a;
-        d1.b = b;
-        d1.dividir(a, b);
-        System.out.println("el resultado es :" + d1.dividir(a, b));
 
-        // se cierra el metodo mian
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Ingrese el primer número:");
+        int a = input.nextInt();
+
+        System.out.println("Ingrese el segundo número:");
+        int b = input.nextInt();
+
+        // Crear una instancia de la clase division
+        Division division = new Division();
+
+        // Establecer los valores de a y b en la instancia de division
+        division.setA(a);
+        division.setB(b);
+
+        // Realizar la division usando el método division de la instancia de division
+        division.dividir();
+
+        // Obtener el resultado de la instancia de division
+        int r = division.getR();
+
+        System.out.println("El resultado es: " + r);
+
+        // Mostrar la representación de la instancia de division
+        System.out.println(division.toString());
     }
-    // se cierra clase
 }
